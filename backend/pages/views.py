600b3255a443c96708ac95aa0566e58ff44bc49a
John O'Keeffe <john.ok.dev@gmail.com>
2024-04-11 19:59:25 +0200

    Add ability to make a POST request.

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, "home.html")
+    return render(
+        request,
+        "home.html",
+        {"new_bird_name": request.POST.get("bird_name", "")}
+        )
 
 
